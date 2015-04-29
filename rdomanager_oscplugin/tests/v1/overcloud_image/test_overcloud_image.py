60745d8dce7dc60c13b1baf1d671d71d4dfd2622
Brad P. Crochet <brad@redhat.com>
2015-04-29 11:53:21 -0400

    Implementation of overcloud image build
    Change-Id: I521596e475a214263fb47c3d2ddb973ebf14394a

--- rdomanager_oscplugin/tests/v1/overcloud_image/test_overcloud_image.py
+++ rdomanager_oscplugin/tests/v1/overcloud_image/test_overcloud_image.py
@@ -14,16 +14,9 @@
 #
 
 import mock
+
 from rdomanager_oscplugin.tests.v1.test_plugin import TestPluginV1
-
-# Load the plugin init module for the plugin list and show commands
 from rdomanager_oscplugin.v1 import overcloud_image
-
-
-class FakePluginV1Client(object):
-    def __init__(self, **kwargs):
-        self.auth_token = kwargs['token']
-        self.management_url = kwargs['endpoint']
 
 
 class TestOvercloudImageBuild(TestPluginV1):
@@ -32,7 +25,125 @@ class TestOvercloudImageBuild(TestPluginV1):
         super(TestOvercloudImageBuild, self).setUp()
 
         # Get the command object to test
-        self.cmd = overcloud_image.BuildPlugin(self.app, None)
+        self.cmd = overcloud_image.BuildOvercloudImage(self.app, None)
+        self.cmd._disk_image_create = mock.Mock()
+        self.cmd._ramdisk_image_create = mock.Mock()
+
+    @mock.patch.object(overcloud_image.BuildOvercloudImage,
+                       '_build_image_fedora_user')
+    def test_overcloud_image_build_all(self, mock_fedora_user):
+        arglist = ['--all']
+        verifylist = [('all', True)]
+
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+
+        self.cmd.take_action(parsed_args)
+
+        self.assertEqual(2, self.cmd._ramdisk_image_create.call_count)
+        self.assertEqual(1, self.cmd._disk_image_create.call_count)
+        self.assertEqual(1, mock_fedora_user.call_count)
+
+    @mock.patch('subprocess.call')
+    @mock.patch('os.path.isfile')
+    @mock.patch('os.chmod')
+    @mock.patch('requests.get')
+    def test_overcloud_image_build_fedora_user_no_cache(
+            self,
+            mock_requests_get,
+            mock_os_chmod,
+            mock_os_path_isfile,
+            mock_subprocess_call):
+        arglist = ['--type', 'fedora-user']
+        verifylist = [('image_types', ['fedora-user'])]
+
+        def os_path_isfile_side_effect(arg):
+            return {
+                'fedora-user.qcow2': False,
+                '~/.cache/image-create/fedora-21.x86_64.qcow2': False,
+            }[arg]
+
+        mock_os_path_isfile.side_effect = os_path_isfile_side_effect
+        requests_get_response = mock.Mock(spec="content")
+        requests_get_response.content = "FEDORAIMAGE".encode('utf-8')
+        mock_requests_get.return_value = requests_get_response
+
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+        mock_open_context = mock.mock_open()
+        mock_open_context().readline.return_value = "Red Hat Enterprise Linux"
+
+        with mock.patch('six.moves.builtins.open', mock_open_context):
+            self.cmd.take_action(parsed_args)
+
+        mock_requests_get.assert_called_once_with(
+            'http://cloud.fedoraproject.org/fedora-21.x86_64.qcow2')
+        self.assertEqual(2, mock_os_path_isfile.call_count)
+        self.assertEqual(1, mock_os_chmod.call_count)
+        mock_open_context.assert_has_calls(
+            [mock.call('fedora-user.qcow2', 'wb')])
+
+    @mock.patch('os.path.isfile')
+    def test_overcloud_image_build_overcloud_full(
+            self,
+            mock_os_path_isfile):
+        arglist = ['--type', 'overcloud-full']
+        verifylist = [('image_types', ['overcloud-full'])]
+
+        def os_path_isfile_side_effect(arg):
+            return {
+                'overcloud-full.qcow2': False,
+            }[arg]
+
+        mock_os_path_isfile.side_effect = os_path_isfile_side_effect
+
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+
+        mock_open_context = mock.mock_open()
+        mock_open_context().readline.return_value = "Red Hat Enterprise Linux"
+
+        with mock.patch('six.moves.builtins.open', mock_open_context):
+            self.cmd.take_action(parsed_args)
+
+        self.cmd._disk_image_create.assert_called_once_with(
+            "-a amd64 -o "
+            "overcloud-full.qcow2 rhel7 overcloud-full overcloud-controller "
+            "overcloud-compute overcloud-ceph-storage sysctl hosts baremetal "
+            "dhcp-all-interfaces os-collect-config heat-config-puppet "
+            "heat-config-script puppet-modules hiera os-net-config "
+            "delorean-repo stable-interface-names grub2 "
+            "-p python-psutil,python-debtcollector selinux-permissive "
+            "element-manifest network-gateway epel rdo-release "
+            "undercloud-package-install "
+            "pip-and-virtualenv-override 2>&1 | tee dib-overcloud-full.log")
+
+    @mock.patch('os.path.isfile')
+    def test_overcloud_image_build_deploy_ramdisk(
+            self,
+            mock_os_path_isfile):
+        arglist = ['--type', 'deploy-ramdisk']
+        verifylist = [('image_types', ['deploy-ramdisk'])]
+
+        def os_path_isfile_side_effect(arg):
+            return {
+                'deploy-ramdisk-ironic.initramfs': False,
+                'deploy-ramdisk-ironic.kernel': False,
+            }[arg]
+
+        mock_os_path_isfile.side_effect = os_path_isfile_side_effect
+
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+
+        mock_open_context = mock.mock_open()
+        mock_open_context().readline.return_value = "Red Hat Enterprise Linux"
+
+        with mock.patch('six.moves.builtins.open', mock_open_context):
+            self.cmd.take_action(parsed_args)
+
+        self.cmd._ramdisk_image_create.assert_called_once_with(
+            "-a amd64 -o deploy-ramdisk-ironic --ramdisk-element "
+            "dracut-ramdisk rhel7 deploy-ironic selinux-permissive "
+            "element-manifest network-gateway epel rdo-release "
+            "undercloud-package-install "
+            "pip-and-virtualenv-override 2>&1 | tee dib-deploy.log")
 
 
 class TestOvercloudImageCreate(TestPluginV1):
--- rdomanager_oscplugin/v1/overcloud_image.py
+++ rdomanager_oscplugin/v1/overcloud_image.py
@@ -13,10 +13,12 @@
 #   under the License.
 #
 
-"""Plugin action implementation"""
-
 import logging
 import os
+import re
+import requests
+import shutil
+import stat
 import subprocess
 
 from cliff import command
@@ -24,15 +26,374 @@ from openstackclient.common import exceptions
 from openstackclient.common import utils
 
 
-class BuildPlugin(command.Command):
-    """Overcloud Image Build plugin"""
+class BuildOvercloudImage(command.Command):
+    """Build images for the overcloud"""
 
     auth_required = False
-    log = logging.getLogger(__name__ + ".BuildPlugin")
+    log = logging.getLogger(__name__ + ".BuildOvercloudImage")
+
+    TRIPLEOPUPPETELEMENTS = "/usr/share/tripleo-puppet-elements"
+    INSTACKUNDERCLOUDELEMENTS = "/usr/share/instack-undercloud"
+    PUPPET_COMMON_ELEMENTS = [
+        'sysctl',
+        'hosts',
+        'baremetal',
+        'dhcp-all-interfaces',
+        'os-collect-config',
+        'heat-config-puppet',
+        'heat-config-script',
+        'puppet-modules',
+        'hiera',
+        'os-net-config',
+        'delorean-repo',
+        'stable-interface-names',
+        'grub2',
+        '-p python-psutil,python-debtcollector',
+    ]
+
+    OVERCLOUD_FULL_DIB_EXTRA_ARGS = [
+        'overcloud-full',
+        'overcloud-controller',
+        'overcloud-compute',
+        'overcloud-ceph-storage',
+    ] + PUPPET_COMMON_ELEMENTS
+
+    DISCOVERY_IMAGE_ELEMENT = [
+        'ironic-discoverd-ramdisk-instack',
+        'delorean-rdo-management',
+    ]
+
+    DEPLOY_IMAGE_ELEMENT = [
+        'deploy-ironic'
+    ]
+
+    IMAGE_TYPES = [
+        'deploy-ramdisk',
+        'discovery-ramdisk',
+        'fedora-user',
+        'overcloud-full',
+    ]
+
+    def get_parser(self, prog_name):
+        parser = super(BuildOvercloudImage, self).get_parser(prog_name)
+        image_group = parser.add_mutually_exclusive_group(required=True)
+        image_group.add_argument(
+            "--all",
+            dest="all",
+            action="store_true",
+            help="Build all images",
+        )
+        image_group.add_argument(
+            "--type",
+            dest="image_types",
+            metavar='<image type>',
+            choices=self.IMAGE_TYPES,
+            action="append",
+            help="Build image by name. One of "
+                 "%s" % ", ".join(self.IMAGE_TYPES),
+        )
+        parser.add_argument(
+            "--base-image",
+            help="Image file to use as a base for new images",
+        )
+        parser.add_argument(
+            "--instack-undercloud-elements",
+            dest="instack_undercloud_elements",
+            default=os.environ.get(
+                "INSTACKUNDERCLOUDELEMENTS", self.INSTACKUNDERCLOUDELEMENTS),
+            help="Path to Instack Undercloud elements",
+        )
+        parser.add_argument(
+            "--tripleo-puppet-elements",
+            dest="tripleo_puppet_elements",
+            default=os.environ.get(
+                "TRIPLEOPUPPETELEMENTS", self.TRIPLEOPUPPETELEMENTS),
+            help="Path to TripleO Puppet elements",
+        )
+        parser.add_argument(
+            "--elements-path",
+            dest="elements_path",
+            default=os.environ.get(
+                "ELEMENTS_PATH",
+                os.pathsep.join([
+                    self.TRIPLEOPUPPETELEMENTS,
+                    self.INSTACKUNDERCLOUDELEMENTS,
+                    '/usr/share/tripleo-image-elements',
+                    '/usr/share/diskimage-builder/elements',
+                    '/usr/share/openstack-heat-templates/'
+                    'software-config/elements',
+                ])),
+            help="Full elements path, separated by %s" % os.pathsep,
+        )
+        parser.add_argument(
+            "--tmp-dir",
+            dest="tmp_dir",
+            default=os.environ.get("TMP_DIR", "/var/tmp"),
+            help="Path to a temporary directory for creating images",
+        )
+        parser.add_argument(
+            "--node-arch",
+            dest="node_arch",
+            default=os.environ.get("NODE_ARCH", "amd64"),
+            help="Architecture of image to build",
+        )
+        parser.add_argument(
+            "--node-dist",
+            dest="node_dist",
+            default=os.environ.get("NODE_DIST", ""),
+            help="Distribution of image to build",
+        )
+        parser.add_argument(
+            "--registration-method",
+            dest="reg_method",
+            default=os.environ.get("REG_METHOD", "disable"),
+            help="Registration method",
+        )
+        parser.add_argument(
+            "--run-rhos-release",
+            dest="run_rhos_release",
+            action='store_true',
+            help="Use RHOS release for repo management (debug only)"
+        )
+        parser.add_argument(
+            "--delorean-trunk-repo",
+            dest="delorean_trunk_repo",
+            default=os.environ.get(
+                'DELOREAN_TRUNK_REPO',
+                'http://trunk.rdoproject.org/kilo/centos7/latest-RDO-kilo-CI/'
+            ),
+            help="URL to Delorean trunk repo",
+        )
+        parser.add_argument(
+            "--overcloud-full-dib-extra-args",
+            dest="overcloud_full_dib_extra_args",
+            default=os.environ.get(
+                "OVERCLOUD_FULL_DIB_EXTRA_ARGS",
+                " ".join(self.OVERCLOUD_FULL_DIB_EXTRA_ARGS)),
+            help="Extra args for Overcloud Full",
+        )
+        parser.add_argument(
+            "--overcloud-full-name",
+            dest="overcloud_full_name",
+            default=os.environ.get('OVERCLOUD_FULL_NAME', 'overcloud-full'),
+            help="Name of overcloud full image",
+        )
+        parser.add_argument(
+            "--fedora-user-name",
+            dest="fedora_user_name",
+            default=os.environ.get('FEDORA_USER_NAME', 'fedora-user'),
+            help="Name of Fedora user image",
+        )
+        parser.add_argument(
+            "--deploy-name",
+            dest="deploy_name",
+            default=os.environ.get('DEPLOY_NAME', 'deploy-ramdisk-ironic'),
+            help="Name of deployment ramdisk image",
+        )
+        parser.add_argument(
+            "--discovery-name",
+            dest="discovery_name",
+            default=os.environ.get('DISCOVERY_NAME', 'discovery-ramdisk'),
+            help="Name of discovery ramdisk image",
+        )
+        parser.add_argument(
+            "--deploy-image-element",
+            dest="deploy_image_element",
+            default=os.environ.get(
+                'DEPLOY_IMAGE_ELEMENT',
+                " ".join(self.DEPLOY_IMAGE_ELEMENT)),
+            help="DIB elements for deploy image",
+        )
+        parser.add_argument(
+            "--discovery-image-element",
+            dest="discovery_image_element",
+            default=os.environ.get(
+                'DISCOVERY_IMAGE_ELEMENT',
+                " ".join(self.DISCOVERY_IMAGE_ELEMENT)),
+            help="DIB elements for discovery image",
+        )
+        return parser
+
+    def _disk_image_create(self, args):
+        subprocess.call('disk-image-create {0}'.format(args), shell=True)
+
+    def _ramdisk_image_create(self, args):
+        subprocess.call('ramdisk-image-create {0}'.format(args), shell=True)
+
+    def _env_var_or_set(self, key_name, default_value):
+        os.environ[key_name] = os.environ.get(key_name, default_value)
+
+    def _prepare_env_variables(self, parsed_args):
+        self._env_var_or_set('ELEMENTS_PATH', parsed_args.elements_path)
+        self._env_var_or_set('TMP_DIR', parsed_args.tmp_dir)
+        self._env_var_or_set(
+            'DIB_REPOREF_puppetlabs_concat',
+            '15ecb98dc3a551024b0b92c6aafdefe960a4596f')
+
+        self._env_var_or_set('DIB_INSTALLTYPE_puppet_modules', 'source')
+
+        # Attempt to detect host distribution if not specified
+        if not parsed_args.node_dist:
+            with open('/etc/redhat-release', 'r') as f:
+                release = f.readline()
+            if re.match('Red Hat Enterprise Linux', release):
+                parsed_args.node_dist = 'rhel7'
+            elif re.match('CentOS', release):
+                parsed_args.node_dist = 'centos7'
+            elif re.match('Fedora', release):
+                parsed_args.node_dist = 'fedora'
+            else:
+                raise Exception(
+                    "Could not detect distribution from "
+                    "/etc/redhat-release!")
+
+        dib_common_elements = []
+        if re.match('rhel7', parsed_args.node_dist):
+            os.environ['REG_METHOD'] = parsed_args.reg_method
+            os.environ['RHOS'] = '0'
+
+            if parsed_args.run_rhos_release:
+                self._env_var_or_set('RHOS_RELEASE', '6')
+                dib_common_elements.append('rhos-release')
+            else:
+                dib_common_elements.append('selinux-permissive')
+            os.environ['DELOREAN_REPO_URL'] = parsed_args.delorean_trunk_repo
+        elif re.match('centos7', parsed_args.node_dist):
+            os.environ['DELOREAN_REPO_URL'] = parsed_args.delorean_trunk_repo
+            dib_common_elements.extend([
+                'selinux-permissive',
+                'centos-cloud-repo',
+            ])
+
+            parsed_args.discovery_image_element = " ".join([
+                'delorean-rdo-management',
+                'ironic-discoverd-ramdisk-instack',
+                'centos-cr',
+            ])
+
+        dib_common_elements.extend([
+            'element-manifest',
+            'network-gateway',
+        ])
+
+        self._env_var_or_set('RHOS', '0')
+        self._env_var_or_set('RHOS_RELEASE', '0')
+
+        if parsed_args.node_dist in ['rhel7', 'centos7']:
+            self._env_var_or_set('FS_TYPE', 'xfs')
+
+            if os.environ.get('RHOS') == '0':
+                os.environ['RDO_RELEASE'] = 'kilo'
+                dib_common_elements.extend([
+                    'epel',
+                    'rdo-release',
+                ])
+            elif not os.environ.get('RHOS_RELEASE') == '0':
+                dib_common_elements.append('rhos-release')
+
+        self._env_var_or_set('PACKAGES', '1')
+        if os.environ.get('PACKAGES') == '1':
+            dib_common_elements.extend([
+                'undercloud-package-install',
+                'pip-and-virtualenv-override',
+            ])
+
+        parsed_args.dib_common_elements = " ".join(dib_common_elements)
+
+    def _build_image_ramdisk(self, parsed_args, ramdisk_type):
+        image_name = vars(parsed_args)["%s_name" % ramdisk_type]
+        if (not os.path.isfile("%s.initramfs" % image_name) or
+           not os.path.isfile("%s.kernel" % image_name)):
+            args = ("-a %(arch)s -o %(name)s "
+                    "--ramdisk-element dracut-ramdisk %(node_dist)s "
+                    "%(image_element)s %(dib_common_elements)s "
+                    "2>&1 | tee dib-%(ramdisk_type)s.log" %
+                    {
+                        'arch': parsed_args.node_arch,
+                        'name': image_name,
+                        'node_dist': parsed_args.node_dist,
+                        'image_element':
+                            vars(parsed_args)["%s_image_element" %
+                                              ramdisk_type],
+                        'dib_common_elements':
+                            parsed_args.dib_common_elements,
+                        'ramdisk_type': ramdisk_type,
+                    })
+            self._ramdisk_image_create(args)
+
+    def _build_image_ramdisks(self, parsed_args):
+        self._build_image_ramdisk_deploy(parsed_args)
+        self._build_image_ramdisk_discovery(parsed_args)
+
+    def _build_image_ramdisk_deploy(self, parsed_args):
+        self._build_image_ramdisk(parsed_args, 'deploy')
+
+    def _build_image_ramdisk_discovery(self, parsed_args):
+        self._build_image_ramdisk(parsed_args, 'discovery')
+
+    def _build_image_overcloud(self, parsed_args, node_type):
+        image_name = "%s.qcow2" % vars(parsed_args)['overcloud_%s_name' %
+                                                    node_type]
+        if not os.path.isfile(image_name):
+            args = ("-a %(arch)s -o %(name)s "
+                    "%(node_dist)s %(overcloud_dib_extra_args)s "
+                    "%(dib_common_elements)s 2>&1 | "
+                    "tee dib-overcloud-%(image_type)s.log" %
+                    {
+                        'arch': parsed_args.node_arch,
+                        'name': image_name,
+                        'node_dist': parsed_args.node_dist,
+                        'overcloud_dib_extra_args':
+                            vars(parsed_args)["overcloud_%s_dib_extra_args" %
+                                              node_type],
+                        'dib_common_elements':
+                            parsed_args.dib_common_elements,
+                        'image_type': node_type,
+                    })
+            self._disk_image_create(args)
+
+    def _build_image_overcloud_full(self, parsed_args):
+        self._build_image_overcloud(parsed_args, 'full')
+
+    def _build_image_fedora_user(self, parsed_args):
+        image_name = "%s.qcow2" % parsed_args.fedora_user_name
+        if not os.path.isfile(image_name):
+            if os.path.isfile('~/.cache/image-create/fedora-21.x86_64.qcow2'):
+                # Just copy the already downloaded Fedora cloud image as
+                # fedora-user.qcow2
+                shutil.copy2(
+                    '~/.cache/image-create/fedora-21.x86_64.qcow2',
+                    image_name)
+            else:
+                # Download the image
+                r = requests.get(
+                    'http://cloud.fedoraproject.org/fedora-21.x86_64.qcow2')
+                with open(image_name, 'wb') as f:
+                    f.write(r.content)
+            # The perms always seem to be wrong when copying out of the cache,
+            # so fix them
+            os.chmod(
+                image_name,
+                stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH)
 
     def take_action(self, parsed_args):
         self.log.debug("take_action(%s)" % parsed_args)
-        pass
+
+        self._prepare_env_variables(parsed_args)
+        self.log.debug("Environment: %s" % os.environ)
+
+        if parsed_args.all:
+            self._build_image_ramdisks(parsed_args)
+            self._build_image_overcloud_full(parsed_args)
+            self._build_image_fedora_user(parsed_args)
+        else:
+            for image_type in parsed_args.image_types:
+                {
+                    'deploy-ramdisk': self._build_image_ramdisk_deploy,
+                    'discovery-ramdisk': self._build_image_ramdisk_discovery,
+                    'fedora-user': self._build_image_fedora_user,
+                    'overcloud-full': self._build_image_overcloud_full,
+                }[image_type](parsed_args)
 
 
 class CreateOvercloud(command.Command):
