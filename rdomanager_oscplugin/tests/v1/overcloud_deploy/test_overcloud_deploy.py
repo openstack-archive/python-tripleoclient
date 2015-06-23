7983638a46c20ae926ee69b05b8021509e643b1f
John Trowbridge <trown@redhat.com>
2015-06-23 16:48:19 -0400

    Pass extra heat templates like heatclient
    Currently, to pass extra heat templates to the deployment,
    all of the templates need to be placed in a subdirectory
    of /etc/tripleo/extra_config.d/, and they must either be
    named *environment*.yaml or *registry*.yaml. This was done
    specifically so we could just drop the rhel-registration
    templates directory from tripleo-heat-templates in our
    extra config drop directory, and it would just work.
    
    However, after trying to explain how this interface works
    to folks adding other extra configuration, it seems like
    this was not the best option. Instead of the drop directory
    approach, we just pass all extra configuration with
    individual -e options, exactly how it is done via heatclient.
    
    Change-Id: Ic0f6a69dd7f71d2777bbf6f07f1a21e076064553

--- rdomanager_oscplugin/tests/v1/overcloud_deploy/test_overcloud_deploy.py
+++ rdomanager_oscplugin/tests/v1/overcloud_deploy/test_overcloud_deploy.py
@@ -220,6 +220,115 @@ class TestDeployOvercloud(fakes.TestDeployOvercloud):
         )
 
     @mock.patch('rdomanager_oscplugin.v1.overcloud_deploy.DeployOvercloud.'
+                '_deploy_postconfig')
+    @mock.patch('rdomanager_oscplugin.v1.overcloud_deploy.DeployOvercloud.'
+                '_update_nodesjson')
+    @mock.patch('rdomanager_oscplugin.utils.get_config_value', autospec=True)
+    @mock.patch('rdomanager_oscplugin.utils.generate_overcloud_passwords')
+    @mock.patch('heatclient.common.template_utils.'
+                'process_multiple_environments_and_files')
+    @mock.patch('heatclient.common.template_utils.get_template_contents')
+    @mock.patch('rdomanager_oscplugin.v1.overcloud_deploy.DeployOvercloud.'
+                '_get_stack')
+    @mock.patch('rdomanager_oscplugin.v1.overcloud_deploy.DeployOvercloud.'
+                '_pre_heat_deploy')
+    @mock.patch('rdomanager_oscplugin.v1.overcloud_deploy.DeployOvercloud.'
+                '_create_overcloudrc')
+    @mock.patch('rdomanager_oscplugin.v1.overcloud_deploy.DeployOvercloud.'
+                '_heat_deploy')
+    def test_tuskar_deploy_extra_config(self, mock_heat_deploy,
+                                        mock_create_overcloudrc,
+                                        most_pre_deploy, mock_get_stack,
+                                        mock_get_templte_contents,
+                                        mock_process_multiple_env,
+                                        mock_generate_overcloud_passwords,
+                                        mock_get_key, mock_update_nodesjson,
+                                        mock_deploy_postconfig):
+
+        arglist = ['--plan', 'undercloud', '--output-dir', 'fake',
+                   '--compute-flavor', 'baremetal',
+                   '--neutron-bridge-mappings', 'datacentre:br-test',
+                   '--neutron-disable-tunneling',
+                   '--control-scale', '3',
+                   '-e', 'extra_registry.yaml',
+                   '-e', 'extra_environment.yaml']
+
+        verifylist = [
+            ('use_tht', False),
+            ('plan', 'undercloud'),
+            ('output_dir', 'fake'),
+            ('extra_templates', ['extra_registry.yaml',
+                                 'extra_environment.yaml'])
+        ]
+
+        clients = self.app.client_manager
+        management = clients.rdomanager_oscplugin.management()
+
+        management.plans.templates.return_value = {}
+        management.plans.resource_class = Plan
+
+        mock_plan = mock.Mock()
+        mock_plan.configure_mock(name="undercloud")
+        management.plans.list.return_value = [mock_plan, ]
+
+        mock_get_templte_contents.return_value = ({}, "template")
+        mock_process_multiple_env.return_value = ({}, "envs")
+        clients.network.api.find_attr.return_value = {
+            "id": "network id"
+        }
+
+        mock_get_key.return_value = "PASSWORD"
+
+        mock_generate_overcloud_passwords.return_value = self._get_passwords()
+
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+
+        self.cmd.take_action(parsed_args)
+
+        parameters = {
+            'Controller-1::SnmpdReadonlyUserPassword': "PASSWORD",
+            'Compute-1::NeutronPassword': 'password',
+            'Controller-1::NeutronPassword': 'password',
+            'Cinder-Storage-1::SnmpdReadonlyUserPassword': "PASSWORD",
+            'Compute-1::CeilometerMeteringSecret': 'password',
+            'NeutronControlPlaneID': 'network id',
+            'Compute-1::NeutronBridgeMappings': 'datacentre:br-test',
+            'Controller-1::AdminPassword': 'password',
+            'Compute-1::Flavor': 'baremetal',
+            'Compute-1::SnmpdReadonlyUserPassword': "PASSWORD",
+            'Controller-1::NeutronEnableTunnelling': False,
+            'Compute-1::NeutronEnableTunnelling': False,
+            'Controller-1::count': 3,
+            'Compute-1::CeilometerPassword': 'password',
+            'Controller-1::CinderPassword': 'password',
+            'Controller-1::CeilometerPassword': 'password',
+            'Compute-1::AdminPassword': 'password',
+            'Controller-1::HeatPassword': 'password',
+            'Controller-1::CeilometerMeteringSecret': 'password',
+            'Controller-1::SwiftPassword': 'password',
+            'Controller-1::NeutronBridgeMappings': 'datacentre:br-test',
+            'Controller-1::NovaPassword': 'password',
+            'Controller-1::SwiftHashSuffix': 'password',
+            'Compute-1::NovaPassword': 'password',
+            'Controller-1::GlancePassword': 'password',
+            'Swift-Storage-1::SnmpdReadonlyUserPassword': "PASSWORD",
+            'Controller-1::AdminToken': 'password',
+            'Controller-1::NeutronL3HA': True,
+            'Controller-1::NeutronAllowL3AgentFailover': False,
+            'Compute-1::NeutronL3HA': True,
+            'Compute-1::NeutronAllowL3AgentFailover': False,
+        }
+
+        mock_heat_deploy.assert_called_with(
+            mock_get_stack(),
+            'fake/plan.yaml',
+            parameters,
+            ['fake/environment.yaml',
+             'extra_registry.yaml',
+             'extra_environment.yaml']
+        )
+
+    @mock.patch('rdomanager_oscplugin.v1.overcloud_deploy.DeployOvercloud.'
                 '_deploy_tuskar', autospec=True)
     @mock.patch('rdomanager_oscplugin.v1.overcloud_deploy.DeployOvercloud.'
                 '_deploy_tripleo_heat_templates', autospec=True)
@@ -240,17 +349,3 @@ class TestDeployOvercloud(fakes.TestDeployOvercloud):
 
         self.assertFalse(mock_deploy_tht.called)
         self.assertFalse(mock_deploy_tuskar.called)
-
-    @mock.patch('rdomanager_oscplugin.v1.overcloud_deploy.glob.os.listdir')
-    def test_get_extra_config(self, os_mock):
-        fake_conf_dir = '/etc/tripleo/extra_config.d'
-        fake_reg_file = fake_conf_dir + '/cool_config/cool.registry.yaml'
-        fake_env_file = fake_conf_dir + '/cool_config/environment.yaml'
-        fake_extra_file = fake_conf_dir + '/cool_config/meow.tar.gz'
-        os_mock.return_value = ([fake_reg_file, fake_env_file,
-                                 fake_extra_file])
-
-        extra_list = self.cmd._get_extra_config(fake_conf_dir)
-        self.assertIn(fake_reg_file, extra_list)
-        self.assertIn(fake_env_file, extra_list)
-        self.assertNotIn(fake_extra_file, extra_list)
