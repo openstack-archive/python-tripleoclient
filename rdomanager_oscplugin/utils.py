rdo-management/python-rdomanager-oscplugin

Check available nodes count before deployment
Before creating or scaling overcloud, number of available nodes
is now checked instead of waiting on stack failed (caused by lack
of available nodes).

This is just a basic check which doesn't ensure
that overcloud creation/scale really passes - it still possible
some that nova scheduler fails to use some of nodes because of
flavor mismatch (for more sophisticated check nova API would
have to be extended).

Change-Id: I1348624ace69a2a24b8ff1d8818fb08de6b8e4d9
Closes-Bug: rhbz#1231777

rdomanager_oscplugin/utils.py
@@ -26,6 +26,8 @@ import sys
 import time
 import uuid
 
+from rdomanager_oscplugin import exceptions
+
 
 WEBROOT = '/dashboard/'
 
@@ -381,3 +383,28 @@ def run_shell(cmd):
 def all_unique(x):
     """Return True if the collection has no duplications."""
     return len(set(x)) == len(x)
+
+
+def check_nodes_count(baremetal_client, stack, parameters, defaults):
+    """Check if there are enough available nodes for creating/scaling stack"""
+    count = 0
+    if stack:
+        for param in defaults:
+            try:
+                current = int(stack.parameters[param])
+            except KeyError:
+                raise ValueError(
+                    "Parameter '%s' was not found in existing stack" % param)
+            count += parameters.get(param, current)
+    else:
+        for param, default in defaults.iteritems():
+            count += parameters.get(param, default)
+
+    available = len(baremetal_client.node.list(associated=False,
+                                               maintenance=False))
+    if count > available:
+        raise exceptions.DeploymentError(
+            "Not enough nodes - available: {0}, requested: {1}".format(
+                available, count))
+    else:
+        return True

openstack-archive/python-tripleoclient

rdomanager_oscplugin/utils.py
@@ -26,6 +26,8 @@ import sys
 import time
 import uuid
 
+from rdomanager_oscplugin import exceptions
+
 
 WEBROOT = '/dashboard/'
 
@@ -395,3 +397,28 @@ def file_checksum(filepath):
         for fragment in iter(lambda: f.read(65536), ''):
             checksum.update(fragment)
     return checksum.hexdigest()
+
+
+def check_nodes_count(baremetal_client, stack, parameters, defaults):
+    """Check if there are enough available nodes for creating/scaling stack"""
+    count = 0
+    if stack:
+        for param in defaults:
+            try:
+                current = int(stack.parameters[param])
+            except KeyError:
+                raise ValueError(
+                    "Parameter '%s' was not found in existing stack" % param)
+            count += parameters.get(param, current)
+    else:
+        for param, default in defaults.items():
+            count += parameters.get(param, default)
+
+    available = len(baremetal_client.node.list(associated=False,
+                                               maintenance=False))
+    if count > available:
+        raise exceptions.DeploymentError(
+            "Not enough nodes - available: {0}, requested: {1}".format(
+                available, count))
+    else:
+        return True