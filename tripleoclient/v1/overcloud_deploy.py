4764f0d56b91a91dd170aa216188f8fff53ad442
marios <marios@redhat.com>
2015-09-08 19:28:05 +0300

    Set the number for DhcpAgentsPerNetwork to controllers num or 3
    Changes the logic which sets DhcpAgentsPerNetwork so that it goes
    to the smallest of controllers number or 3.
    
    Ported from original review by gfidente @
    https://review.gerrithub.io/#/c/246108/
    
    Change-Id: I187795ff27296e478cc700fc1beb7af58a1473b1
    Closes-Bug: 1493445

--- tripleoclient/v1/overcloud_deploy.py
+++ tripleoclient/v1/overcloud_deploy.py
@@ -344,9 +344,8 @@ class DeployOvercloud(command.Command):
                     'Compute-1::NeutronAllowL3AgentFailover': False,
                 })
 
-        # set at least 3 dhcp_agents_per_network
-        dhcp_agents_per_network = (number_controllers if number_controllers and
-                                   number_controllers > 3 else 3)
+        dhcp_agents_per_network = (min(number_controllers, 3) if
+                                   number_controllers else 1)
 
         if args.templates:
             parameters.update({
