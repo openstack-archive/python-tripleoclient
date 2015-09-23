rdo-management/python-rdomanager-oscplugin

Actually call _validate_args to check for network types mismatch
https://bugzilla.redhat.com/show_bug.cgi?id=1244893

_validate_args was not actually being called

Change-Id: Ie830049ee06a4e0cf2cd57d055aed537b0315870
Resolves: rhbz#1244893

rdomanager_oscplugin/tests/v1/overcloud_deploy/test_overcloud_deploy.py
@@ -181,6 +181,8 @@ class TestDeployOvercloud(fakes.TestDeployOvercloud):
         mock_create_tempest_deployer_input.assert_called_with(self.cmd)
 
     @mock.patch('rdomanager_oscplugin.v1.overcloud_deploy.DeployOvercloud.'
+                '_validate_args')
+    @mock.patch('rdomanager_oscplugin.v1.overcloud_deploy.DeployOvercloud.'
                 '_deploy_postconfig')
     @mock.patch('rdomanager_oscplugin.v1.overcloud_deploy.DeployOvercloud.'
                 '_create_tempest_deployer_input', autospec=True)
@@ -217,7 +219,7 @@ class TestDeployOvercloud(fakes.TestDeployOvercloud):
                         mock_create_overcloudrc,
                         mock_generate_overcloud_passwords,
                         mock_create_tempest_deployer_input,
-                        mock_deploy_postconfig):
+                        mock_deploy_postconfig, mock_validate_args):
 
         arglist = ['--templates', '--ceph-storage-scale', '3']
         verifylist = [
@@ -329,6 +331,8 @@ class TestDeployOvercloud(fakes.TestDeployOvercloud):
              'mergepy.yaml'))
 
         mock_create_tempest_deployer_input.assert_called_with(self.cmd)
+
+        mock_validate_args.assert_called_once_with(parsed_args)
 
     @mock.patch('rdomanager_oscplugin.v1.overcloud_deploy.DeployOvercloud.'
                 '_deploy_postconfig')

openstack-archive/python-tripleoclient

tripleoclient/tests/v1/overcloud_deploy/test_overcloud_deploy.py
@@ -203,6 +203,8 @@ class TestDeployOvercloud(fakes.TestDeployOvercloud):
             [self.parameter_defaults_env_file])
 
     @mock.patch('tripleoclient.v1.overcloud_deploy.DeployOvercloud.'
+                '_validate_args')
+    @mock.patch('tripleoclient.v1.overcloud_deploy.DeployOvercloud.'
                 '_create_parameters_env')
     @mock.patch('tripleoclient.v1.overcloud_deploy.DeployOvercloud.'
                 '_deploy_postconfig')
@@ -242,7 +244,7 @@ class TestDeployOvercloud(fakes.TestDeployOvercloud):
                         mock_generate_overcloud_passwords,
                         mock_create_tempest_deployer_input,
                         mock_deploy_postconfig,
-                        mock_create_parameters_env):
+                        mock_create_parameters_env, mock_validate_args):
 
         arglist = ['--templates', '--ceph-storage-scale', '3']
         verifylist = [
@@ -364,6 +366,8 @@ class TestDeployOvercloud(fakes.TestDeployOvercloud):
             ['/usr/share/openstack-tripleo-heat-templates/overcloud-resource-'
              'registry-puppet.yaml', '/fake/path',
              self.parameter_defaults_env_file])
+
+        mock_validate_args.assert_called_once_with(parsed_args)
 
     @mock.patch('tripleoclient.v1.overcloud_deploy.DeployOvercloud.'
                 '_deploy_postconfig')