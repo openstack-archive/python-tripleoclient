879d73c5a4c4fda99412a16229931ab91232796d
Brad P. Crochet <brad@redhat.com>
2017-03-23 13:18:04 -0400

    Remove update abort
    The update abort is not reliable, and could mess up a TripleO stack
    beyond repair. Since this has a potential for data loss, I would
    suggest this be removed without a deprecation period.
    
    Change-Id: Ieec4f01e38768eafb3df1f06340bdd3e220d30bd
    Related-Bug: #1668269
    (cherry picked from commit 1d3231de5a74d5ddf89cd96ccb598e2293a36b2f)

--- tripleoclient/workflows/package_update.py
+++ tripleoclient/workflows/package_update.py
@@ -67,24 +67,6 @@ def update_and_wait(log, clients, stack, plan_name, verbose_level,
     return stack.status
 
 
-def abort_update(clients, **workflow_input):
-
-    workflow_client = clients.workflow_engine
-    tripleoclients = clients.tripleoclient
-    workflow_input['queue_name'] = str(uuid.uuid4())
-    queue_name = workflow_input['queue_name']
-
-    execution = base.start_workflow(
-        workflow_client,
-        'tripleo.package_update.v1.cancel_stack_update',
-        workflow_input=workflow_input
-    )
-
-    with tripleoclients.messaging_websocket(queue_name) as ws:
-        message = ws.wait_for_message(execution.id)
-        assert message['status'] == "SUCCESS", pprint.pformat(message)
-
-
 def clear_breakpoints(clients, **workflow_input):
     workflow_client = clients.workflow_engine
     tripleoclients = clients.tripleoclient
