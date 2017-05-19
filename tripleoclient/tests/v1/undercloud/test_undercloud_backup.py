dea52f7afa35c5750924fbdad1971a0e92c2af27
Carlos Camacho <ccamacho@redhat.com>
2017-05-19 09:24:03 +0200

    Add Openstack Undercloud backup
    This new option allows to call the Mistral workflow to run an Undercloud full
    backup.
    
    Usage: openstack undercloud backup [--add-path ADD_FILES_TO_BACKUP]
    
    The --add-path allows to add additional files or folders to the
    Undercloud backup, for example:
    
    openstack undercloud backup --add-path /etc/hosts \
                                --add-path /var/log/
    
    Depends-on: Iebd67568d5e72967e694b88fc8c73361543929a1
    Change-Id: Ief10f5313244d2848b6de604e0493a51db1ed30f
    Related bp undercloud-backup-restore
    (cherry picked from commit eaaf2e8bc568cd71c9d6753ebf3c349a315cf8c1)

---
+++ tripleoclient/tests/v1/undercloud/test_undercloud_backup.py
@@ -0,0 +1,60 @@
+#   Copyright 2018 Red Hat, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
+import mock
+
+from osc_lib.tests import utils
+from tripleoclient.v1 import undercloud_backup
+
+
+class TestUndercloudBackup(utils.TestCommand):
+
+    def setUp(self):
+        super(TestUndercloudBackup, self).setUp()
+
+        # Get the command object to test
+        self.cmd = undercloud_backup.BackupUndercloud(self.app, None)
+        self.app.client_manager.workflow_engine = mock.Mock()
+        self.workflow = self.app.client_manager.workflow_engine
+
+    @mock.patch('tripleoclient.workflows.undercloud_backup.backup',
+                autospec=True)
+    def test_undercloud_backup_noargs(self, plan_mock):
+        arglist = []
+        verifylist = []
+
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+
+        self.cmd.take_action(parsed_args)
+        plan_mock.assert_called_once_with(
+            mock.ANY, {'sources_path': '/home/stack/'})
+
+    @mock.patch('tripleoclient.workflows.undercloud_backup.backup',
+                autospec=True)
+    def test_undercloud_backup_withargs(self, plan_mock):
+        arglist = [
+            '--add-path',
+            '/tmp/foo.yaml',
+            '--add-path',
+            '/tmp/bar.yaml'
+        ]
+        verifylist = []
+
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+
+        self.cmd.take_action(parsed_args)
+        plan_mock.assert_called_once_with(
+            mock.ANY,
+            {'sources_path': '/home/stack/,/tmp/bar.yaml,/tmp/foo.yaml'})
