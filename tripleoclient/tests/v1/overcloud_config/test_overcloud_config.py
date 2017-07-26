8a7da9fe26e8b8db6ee26dfdae940f36c9b55488
Mathieu Bultel <mbultel@redhat.com>
2017-07-26 17:40:46 +0200

    Implement minor update workflow with config download
    This change aim to refactor the way of doing the minor upgrade
    via ansible playbook download by the config download
    It will depend on a mistral change which will get the
    ansible update_task and run it via mistral
    The user will have two ways for performing the minor update:
      - the stack update command which will make an automatic minor
        upgrade (or just upgrade a given set of nodes)
      - running it manually via ansible on the undercloud
    
    Closes-Bug: #1715557
    Closes-Bug: #1723108
    Change-Id: I4fcd443d975894a1da0286b19506d00682c5768c

--- tripleoclient/tests/v1/overcloud_config/test_overcloud_config.py
+++ tripleoclient/tests/v1/overcloud_config/test_overcloud_config.py
@@ -10,15 +10,10 @@
 #   License for the specific language governing permissions and limitations
 #   under the License.
 
-import fixtures
 import mock
-import os
 
-from mock import call
-from mock import patch
 from osc_lib.tests import utils
 
-from tripleoclient.tests.v1.overcloud_config import fakes
 from tripleoclient.v1 import overcloud_config
 
 
@@ -32,138 +27,14 @@ class TestOvercloudConfig(utils.TestCommand):
         self.app.client_manager.orchestration = mock.Mock()
         self.workflow = self.app.client_manager.workflow_engine
 
-    @patch.object(overcloud_config.DownloadConfig, '_mkdir')
-    @patch.object(overcloud_config.DownloadConfig, '_open_file')
-    @mock.patch('tempfile.mkdtemp', autospec=True)
-    def test_overcloud_config_generate_config(self,
-                                              mock_tmpdir,
-                                              mock_open,
-                                              mock_mkdir):
+    @mock.patch('tripleo_common.utils.config.Config.download_config')
+    def test_overcloud_download_config(self, mock_config):
         arglist = ['--name', 'overcloud', '--config-dir', '/tmp']
         verifylist = [
             ('name', 'overcloud'),
             ('config_dir', '/tmp')
         ]
-        config_type_list = ['config_settings', 'global_config_settings',
-                            'logging_sources', 'monitoring_subscriptions',
-                            'service_config_settings',
-                            'service_metadata_settings',
-                            'service_names',
-                            'upgrade_batch_tasks', 'upgrade_tasks']
-        fake_role = [role for role in
-                     fakes.FAKE_STACK['outputs'][1]['output_value']]
 
         parsed_args = self.check_parser(self.cmd, arglist, verifylist)
-        clients = self.app.client_manager
-        orchestration_client = clients.orchestration
-        orchestration_client.stacks.get.return_value = fakes.create_tht_stack()
-        mock_tmpdir.return_value = "/tmp/tht"
         self.cmd.take_action(parsed_args)
-        expected_mkdir_calls = [call('/tmp/tht/%s' % r) for r in fake_role]
-        mock_mkdir.assert_has_calls(expected_mkdir_calls, any_order=True)
-        expected_calls = []
-        for config in config_type_list:
-            for role in fake_role:
-                if config == 'step_config':
-                    expected_calls += [call('/tmp/tht/%s/%s.pp' %
-                                            (role, config))]
-                else:
-                    expected_calls += [call('/tmp/tht/%s/%s.yaml' %
-                                            (role, config))]
-        mock_open.assert_has_calls(expected_calls, any_order=True)
-
-    @patch.object(overcloud_config.DownloadConfig, '_mkdir')
-    @patch.object(overcloud_config.DownloadConfig, '_open_file')
-    @mock.patch('tempfile.mkdtemp', autospec=True)
-    def test_overcloud_config_one_config_type(self,
-                                              mock_tmpdir,
-                                              mock_open,
-                                              mock_mkdir):
-
-        arglist = ['--name', 'overcloud', '--config-dir', '/tmp',
-                   '--config-type', ['config_settings']]
-        verifylist = [
-            ('name', 'overcloud'),
-            ('config_dir', '/tmp'),
-            ('config_type', ['config_settings'])
-        ]
-        expected_config_type = 'config_settings'
-        fake_role = [role for role in
-                     fakes.FAKE_STACK['outputs'][1]['output_value']]
-        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
-
-        clients = self.app.client_manager
-        orchestration_client = clients.orchestration
-        orchestration_client.stacks.get.return_value = fakes.create_tht_stack()
-        mock_tmpdir.return_value = "/tmp/tht"
-        self.cmd.take_action(parsed_args)
-        expected_mkdir_calls = [call('/tmp/tht/%s' % r) for r in fake_role]
-        expected_calls = [call('/tmp/tht/%s/%s.yaml'
-                          % (r, expected_config_type))
-                          for r in fake_role]
-        mock_mkdir.assert_has_calls(expected_mkdir_calls, any_order=True)
-        mock_open.assert_has_calls(expected_calls, any_order=True)
-
-    @mock.patch('os.mkdir')
-    @mock.patch('six.moves.builtins.open')
-    @mock.patch('tempfile.mkdtemp', autospec=True)
-    def test_overcloud_config_wrong_config_type(self, mock_tmpdir,
-                                                mock_open, mock_mkdir):
-
-        arglist = [
-            '--name', 'overcloud',
-            '--config-dir',
-            '/tmp', '--config-type', ['bad_config']]
-        verifylist = [
-            ('name', 'overcloud'),
-            ('config_dir', '/tmp'),
-            ('config_type', ['bad_config'])
-        ]
-        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
-        clients = self.app.client_manager
-        mock_tmpdir.return_value = "/tmp/tht"
-        orchestration_client = clients.orchestration
-        orchestration_client.stacks.get.return_value = fakes.create_tht_stack()
-        self.assertRaises(
-            KeyError,
-            self.cmd.take_action, parsed_args)
-
-    @mock.patch('tripleoclient.utils.get_role_data', autospec=True)
-    def test_overcloud_config_upgrade_tasks(self, mock_get_role_data):
-
-        clients = self.app.client_manager
-        orchestration_client = clients.orchestration
-        orchestration_client.stacks.get.return_value = fakes.create_tht_stack()
-        self.tmp_dir = self.useFixture(fixtures.TempDir()).path
-        fake_role = [role for role in
-                     fakes.FAKE_STACK['outputs'][1]['output_value']]
-        expected_tasks = {'FakeController': [{'name': 'Stop fake service',
-                                              'service': 'name=fake '
-                                              'state=stopped',
-                                              'tags': 'step1',
-                                              'when': 'step|int == 1'}],
-                          'FakeCompute': [{'name': 'Stop fake service',
-                                           'service':
-                                           'name=fake state=stopped',
-                                           'tags': 'step1',
-                                           'when': ['existingcondition',
-                                                    'step|int == 1']},
-                                          {'name': 'Stop nova-'
-                                           'compute service',
-                                           'service':
-                                           'name=openstack-nova-'
-                                           'compute state=stopped',
-                                           'tags': 'step1',
-                                           'when': ['existing',
-                                                    'list', 'step|int == 1']}]}
-        mock_get_role_data.return_value = fake_role
-
-        for role in fake_role:
-            filedir = os.path.join(self.tmp_dir, role)
-            os.makedirs(filedir)
-            filepath = os.path.join(filedir, "upgrade_tasks_playbook.yaml")
-            playbook_tasks = self.cmd._write_playbook_get_tasks(
-                fakes.FAKE_STACK['outputs'][1]['output_value'][role]
-                ['upgrade_tasks'], role, filepath)
-            self.assertTrue(os.path.isfile(filepath))
-            self.assertEqual(expected_tasks[role], playbook_tasks)
+        mock_config.assert_called_once_with('overcloud', '/tmp', None)
