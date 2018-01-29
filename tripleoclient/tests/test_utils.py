8a6e018782aac06e652dfa2d42a9408afb02bbba
Dougal Matthews <dougal@redhat.com>
2018-01-29 14:46:23 +0000

    Revert "Remove the overcloudrc.v3 file"
    This reverts commit 977bc6f1b864c2cee9a92c9de1acee109d5e0195.
    
    The original change is backwards incompatible since it removes the
    overcloudrc v3 file without any deprecation.
    
    Related-Bug: #1733640
    Change-Id: I6b62e014312f7502a693f2990556d58696b1df11

--- tripleoclient/tests/test_utils.py
+++ tripleoclient/tests/test_utils.py
@@ -212,19 +212,25 @@ class TestCreateOvercloudRC(TestCase):
 
         tempdir = tempfile.mkdtemp()
         rcfile = os.path.join(tempdir, 'teststackrc')
+        rcfile_v3 = os.path.join(tempdir, 'teststackrc.v3')
 
         overcloudrcs = {
-            "overcloudrc": "overcloudrc v3 is the only version",
+            "overcloudrc": "overcloudrc not v3",
+            "overcloudrc.v3": "overcloudrc.v3",
         }
 
         try:
             utils.write_overcloudrc(stack_name, overcloudrcs,
                                     config_directory=tempdir)
             rc = open(rcfile, 'rt').read()
-            self.assertIn('overcloudrc v3', rc)
+            self.assertIn('overcloudrc not v3', rc)
+            rc_v3 = open(rcfile_v3, 'rt').read()
+            self.assertIn('overcloudrc.v3', rc_v3)
         finally:
             if os.path.exists(rcfile):
                 os.unlink(rcfile)
+            if os.path.exists(rcfile_v3):
+                os.unlink(rcfile_v3)
 
             os.rmdir(tempdir)
 
