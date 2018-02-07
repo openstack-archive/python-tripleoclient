fbe0ea6fe01e832b35363bbc3b94dd7f713394cf
Rushil Chugh <rushil.chugh@gmail.com>
2018-02-07 01:47:40 -0500

    Add XClarity in the list of supported hw types
    This patch proposes to add XClarity in the list of supported
    hardware types by python-tripleoclient.
    
    Change-Id: If1e32d13836dbb430ca9543edf6873e1c7971fb0

--- tripleoclient/v1/undercloud_config.py
+++ tripleoclient/v1/undercloud_config.py
@@ -413,7 +413,8 @@ def _process_drivers_and_hardware_types(conf, env):
     # use one variable for them.
     mgmt_interfaces = {'fake', 'ipmitool'}
     # TODO(dtantsur): can we somehow avoid hardcoding hardware types here?
-    for hw_type in ('redfish', 'idrac', 'ilo', 'irmc', 'staging-ovirt'):
+    for hw_type in ('redfish', 'idrac', 'ilo', 'irmc', 'staging-ovirt',
+                    'xclarity'):
         if hw_type in enabled_hardware_types:
             mgmt_interfaces.add(hw_type)
     for (hw_type, iface) in [('cisco-ucs-managed', 'ucsm'),
