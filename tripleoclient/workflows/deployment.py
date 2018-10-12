ca1fd76fdd551edd4892a6d87b65369aa179634a
Dougal Matthews <dougal@redhat.com>
2018-10-12 11:00:50 +0100

    Increase the deploy_plan timeout in tripleoclient
    The time for the deploy workflow to complete has been creeping up, and
    it is getting close to the 6 minute timeout in the client. This bumps
    the timeout to 10 minutes.
    
    Change-Id: Iadb7cc9ba3b62a0221109b1dacf2d764944f691a
    Related-Bug: #1789680
    (cherry picked from commit 9f716d8f66640c8ff89929843884dfb9638a96bb)
    (cherry picked from commit 69f8b31201273e587a69ebe5ea5e0ca5af809b68)

--- tripleoclient/workflows/deployment.py
+++ tripleoclient/workflows/deployment.py
@@ -43,10 +43,10 @@ def deploy(clients, **workflow_input):
         )
 
         # The deploy workflow ends once the Heat create/update starts. This
-        # means that is shouldn't take very long. Wait for six minutes for
+        # means that is shouldn't take very long. Wait for 10 minutes for
         # messages from the workflow.
         for payload in base.wait_for_messages(workflow_client, ws, execution,
-                                              360):
+                                              600):
             assert payload['status'] == "SUCCESS", pprint.pformat(payload)
 
 
