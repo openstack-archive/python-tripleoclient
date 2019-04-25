6b0bf9098e5c6d3bc6677e39406a397bb4e50527
Emilien Macchi <emilien@redhat.com>
2019-04-25 15:38:41 -0400

    undercloud: resolve undercloud_*_host
    The problem we're solving here is that our operators using SSL + FQDN
    based endpoints will have failures during the deployment because we
    don't lookup the FQDN into IP addresses, needed later in the deployment
    for proper binding.
    
    This patch transforms undercloud_*_host parameters into IP addresses:
    
    - We raise if lookup returns nothing.
    - We raise if lookup returns more than one IP.
    - We support both IPv4 and IPv6.
    - We raise if the IP is a loopback.
    - We raise if the returned IP is invalid.
    
    Utils changes:
    
    * Introduce utils.is_valid_ip.
      Return True if the IP is either v4 or v6. Return False otherwise.
    
    * Introduce utils.is_loopback.
      Return True if the given host is a loopback. Return False otherwise.
    
    * Introduce utils.get_host_ips.
      Returns a list of IPs for a host to lookup.
    
    * Introduce utils.get_single_ip.
      Translate an hostname or FQDN into an IP address if it is valid IP.
      Return it unchanged if it is an IPv4 or IPv6 address.
      If the host is not reachable, it'll raise an exception.
      By default it excludes the loopbacks but it can be allowed by setting
      allow_loopback = True.
    
    * Use utils.get_single_ip to translate undercloud_admin_host and
      undercloud_public_host to IP addresses.
    
    Related-Bug: #1763776
    Change-Id: Ic008cc758493aa95e8aa237d23c2f66c0a930509
    (cherry picked from commit c925c3b93bf02538c9308baf91aa0c7cc6dd20e9)

--- tripleoclient/v1/undercloud_config.py
+++ tripleoclient/v1/undercloud_config.py
@@ -463,18 +463,19 @@ def prepare_undercloud_deploy(upgrade=False, no_validations=False,
             CONF.get('undercloud_service_certificate')):
         endpoint_environment = _get_tls_endpoint_environment(
             CONF.get('undercloud_public_host'), tht_templates)
-        try:
-            public_host = CONF.get('undercloud_public_host')
-            netaddr.IPAddress(public_host)
-            deploy_args += ['--public-virtual-ip', public_host]
 
-            admin_host = CONF.get('undercloud_admin_host')
-            netaddr.IPAddress(admin_host)
-            deploy_args += ['--control-virtual-ip', admin_host]
-        except netaddr.core.AddrFormatError:
-            # TODO(jaosorior): We could do a reverse lookup for the hostnames
-            # if the *_host variables are DNS names and not IPs.
-            pass
+        public_host = utils.get_single_ip(CONF.get('undercloud_public_host'))
+        netaddr.IPAddress(public_host)
+        deploy_args += ['--public-virtual-ip', public_host]
+
+        # To make sure the resolved host is set to the right IP in /etc/hosts
+        if not utils.is_valid_ip(CONF.get('undercloud_public_host')):
+            extra_host = public_host + ' ' + CONF.get('undercloud_public_host')
+            env_data['ExtraHostFileEntries'] = extra_host
+
+        admin_host = utils.get_single_ip(CONF.get('undercloud_admin_host'))
+        netaddr.IPAddress(admin_host)
+        deploy_args += ['--control-virtual-ip', admin_host]
 
         deploy_args += [
             '-e', endpoint_environment,
