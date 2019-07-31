ac7ae9e8d638676196c4897a30c978d51a091900
yatinkarel <ykarel@redhat.com>
2019-07-31 21:39:38 +0530

    Pass deployment_options to config download
    https://review.opendev.org/673862 allows config_download
    workflow to take deployment_options as a input, this patch
    passes it to the workflow so ansible_python_interpreter
    is set correctly in generated inventory and thus used
    during ansible deployment.
    
    Depends-On: https://review.opendev.org/#/c/673862/
    Closes-Bug: #1838540
    Change-Id: I993fc09fd571fb93d78ffdbecf1823e806f744f4

--- tripleoclient/v1/overcloud_deploy.py
+++ tripleoclient/v1/overcloud_deploy.py
@@ -986,6 +986,11 @@ class DeployOvercloud(command.Command):
                     used = int(time.time() - start)
                     timeout = (parsed_args.timeout * 60) - used
 
+                deployment_options = {}
+                if parsed_args.deployment_python_interpreter:
+                    deployment_options['ansible_python_interpreter'] = \
+                        parsed_args.deployment_python_interpreter
+
                 deployment.config_download(
                     self.log, self.clients, stack,
                     parsed_args.templates, parsed_args.overcloud_ssh_user,
@@ -994,7 +999,8 @@ class DeployOvercloud(command.Command):
                     parsed_args.output_dir,
                     parsed_args.override_ansible_cfg,
                     timeout,
-                    verbosity=self.app_args.verbose_level)
+                    verbosity=self.app_args.verbose_level,
+                    deployment_options=deployment_options)
             except Exception:
                 deployment.set_deployment_status(
                     self.clients, 'failed',
