2e6358a61a4b83b038759300f5307aabcf51788d
Harald Jensås <hjensas@redhat.com>
2019-09-26 14:50:07 +0200

    Set option:mtu in inspector dnsmasq
    Set the mtu for all subnets in inspector dnsmasq
    config to value of 'local_mtu' in undercloud.conf.
    
    Conflicts:
      tripleoclient/tests/v1/undercloud/test_config.py
      tripleoclient/v1/undercloud_config.py
    
    NOTE, tripleoclient/tests/v1/undercloud/test_config.py
    was named test_undercloud_config.py in rocky and the
    relevant unit tests did not exist.
    
    Closes-Bug: #1845487
    Depends-On: Ic95dbf1867fb5397f1b6d8f23466910a97051cb6
    Change-Id: Iafe3156ca0678f3ab3c5e688e712a9fb6b408e7b
    (cherry picked from commit ff31fe18ec028e6d187de2c3fda539d28caf0a27)

--- tripleoclient/v1/undercloud_config.py
+++ tripleoclient/v1/undercloud_config.py
@@ -211,6 +211,7 @@ def _generate_inspection_subnets():
         env_dict['ip_range'] = s.inspection_iprange
         env_dict['netmask'] = str(netaddr.IPNetwork(s.cidr).netmask)
         env_dict['gateway'] = s.gateway
+        env_dict['mtu'] = CONF.local_mtu
         env_list.append(env_dict)
     return env_list
 
