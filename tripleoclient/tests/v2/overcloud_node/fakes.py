73dfcd1b96698774dd6ac7d1cad825e0b41e47e2
Dougal Matthews <dougal@dougalmatthews.com>
2019-11-26 16:22:21 +0000

    Migrate the CLI introspection from Mistral to Ansible
    The baremetal introspection command has been implemented in v2 using
    an ansible playbook instead of a mistral workflow.
    
    Story: 2007212
    Task: 38444
    Task: 38445
    Change-Id: I61a4d1ae241bd3877abbc77bf4bad226d2502379

---
+++ tripleoclient/tests/v2/overcloud_node/fakes.py
@@ -0,0 +1,51 @@
+#   Copyright 2015 Red Hat, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
+import mock
+from osc_lib.tests import utils
+
+
+class FakeClientWrapper(object):
+
+    def __init__(self):
+        self._instance = mock.Mock()
+        self._mock_websocket = mock.Mock()
+        self._mock_websocket.__enter__ = mock.Mock(
+            return_value=self._mock_websocket)
+        # Return False to avoid silencing exceptions
+        self._mock_websocket.__exit__ = mock.Mock(return_value=False)
+
+    def messaging_websocket(self):
+        return self._mock_websocket
+
+
+class TestDeleteNode(utils.TestCommand):
+
+    def setUp(self):
+        super(TestDeleteNode, self).setUp()
+
+        self.app.client_manager.auth_ref = mock.Mock(auth_token="TOKEN")
+        self.app.client_manager.orchestration = mock.Mock()
+        self.app.client_manager.tripleoclient = FakeClientWrapper()
+
+
+class TestOvercloudNode(utils.TestCommand):
+
+    def setUp(self):
+        super(TestOvercloudNode, self).setUp()
+
+        self.app.client_manager.baremetal = mock.Mock()
+        self.app.client_manager.workflow_engine = mock.Mock()
+        self.app.client_manager.tripleoclient = FakeClientWrapper()
