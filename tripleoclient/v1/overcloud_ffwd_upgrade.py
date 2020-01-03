188acfbc40448278084bc05ee8d4ad7ecc61905a
Alex Schultz <aschultz@redhat.com>
2020-01-03 11:46:21 -0700

    Add additional ssh timeout options
    Added `--overcloud-ssh-enable-timeout` and
    `--overcloud-ssh-port-timeout` to the openstack overcloud deploy command
    to allow users the ability to tune these values.  The ssh port timeout
    might need to be tuned if the hardware being deployed is slow to post
    and takes longer than 5 minutes to become ready.
    
    These options have been added to:
    
     * openstack overcloud deploy
     * openstack overcloud admin
     * openstack overcloud ffu upgrade
     * openstack overcloud upgrade
    
    Change-Id: I56f6001b5efeed7ccb3918a5c2622c3eca613c07
    Closes-Bug: #1858222

--- tripleoclient/v1/overcloud_ffwd_upgrade.py
+++ tripleoclient/v1/overcloud_ffwd_upgrade.py
@@ -93,7 +93,9 @@ class FFWDUpgradePrepare(DeployOvercloud):
         stack = oooutils.get_stack(clients.orchestration, parsed_args.stack)
         deployment.get_hosts_and_enable_ssh_admin(
             self.log, clients, stack, parsed_args.overcloud_ssh_network,
-            parsed_args.overcloud_ssh_user, parsed_args.overcloud_ssh_key)
+            parsed_args.overcloud_ssh_user, parsed_args.overcloud_ssh_key,
+            parsed_args.overcloud_ssh_enable_timeout,
+            parsed_args.overcloud_ssh_port_timeout)
 
         self.log.info("FFWD Upgrade Prepare on stack {0} complete.".format(
                       parsed_args.stack))
