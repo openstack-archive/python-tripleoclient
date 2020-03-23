8f5a2dab0717c646b2925f112762febb717d16cf
Kevin Carter <kecarter@redhat.com>
2020-03-23 15:33:26 -0500

    Normalize the 'undercloud-passwords.conf' return data
    This change normalizes our client return formation by ensuring we're always
    using the configured output path as defined within our constants.
    
    Closes-Bug: #1868619
    
    Change-Id: Ied051214c4d3f2a695d58a6cde6180f040ca02d4
    Signed-off-by: Kevin Carter <kecarter@redhat.com>
    (cherry picked from commit 8dc3273495c13924861fd4fca103732f0d48fc2d)

--- tripleoclient/v1/undercloud_preflight.py
+++ tripleoclient/v1/undercloud_preflight.py
@@ -39,8 +39,10 @@ CONF = cfg.CONF
 # We need 8 GB, leave a little room for variation in what 8 GB means on
 # different platforms.
 REQUIRED_MB = 7680
-PASSWORD_PATH = '%s/%s' % (constants.UNDERCLOUD_OUTPUT_DIR,
-                           'undercloud-passwords.conf')
+PASSWORD_PATH = os.path.join(
+    constants.UNDERCLOUD_OUTPUT_DIR,
+    'undercloud-passwords.conf'
+)
 
 LOG = logging.getLogger(__name__ + ".UndercloudSetup")
 
