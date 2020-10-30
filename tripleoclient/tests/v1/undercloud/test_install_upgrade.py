77abd0acb47d90735f4014cf4c0f183505c48d3a
MSA <marios@redhat.com>
2020-10-30 18:11:24 +0200

    Remove detection of package manager for undercloud upgrade
    In [1] we added protection for yum not being available.
    We should just default to dnf for anything newer than
    ussuri.
    
    [1] https://github.com/openstack/python-tripleoclient/commit/9c53cb3ef5ad513b05d4888552a3280e0eec43ca
    
    Change-Id: I5056579ae117c46944f20868c5cd1edfc1b86036
    (cherry picked from commit 4a07e16a5883fc72613e63a95da02332f75226fa)

--- tripleoclient/tests/v1/undercloud/test_install_upgrade.py
+++ tripleoclient/tests/v1/undercloud/test_install_upgrade.py
@@ -575,8 +575,6 @@ class TestUndercloudUpgrade(TestPluginV1):
         app_args.verbose_level = 1
         self.cmd = undercloud.UpgradeUndercloud(self.app, app_args)
 
-    @mock.patch('os.system')
-    @mock.patch('sys.version_info')
     @mock.patch('tripleoclient.utils.prompt_user_for_confirmation',
                 return_value=True)
     @mock.patch.object(sys, 'executable', 'python2')
@@ -591,32 +589,15 @@ class TestUndercloudUpgrade(TestPluginV1):
     def test_undercloud_upgrade_default(self, mock_run_command,
                                         mock_subprocess, mock_wr,
                                         mock_os_mkdir, mock_copy, mock_user,
-                                        mock_getuid, mock_confirm, mock_sys,
-                                        mock_os_sys):
+                                        mock_getuid, mock_confirm):
         arglist = ['--no-validations']
         verifylist = []
         parsed_args = self.check_parser(self.cmd, arglist, verifylist)
 
-        mock_sys.major = 3
-        mock_os_sys.return_value = 0
         # DisplayCommandBase.take_action() returns two tuples
         self.cmd.take_action(parsed_args)
         mock_run_command.assert_called_with(
             ['sudo', 'dnf', 'upgrade', '-y',
-             'python2-tripleoclient',
-             'openstack-tripleo-common',
-             'openstack-tripleo-heat-templates',
-             'openstack-tripleo-validations',
-             'tripleo-ansible'],
-            name='Update extra packages'
-        )
-        mock_os_sys.assert_called_with("which dnf")
-
-        mock_sys.major = 2
-        # DisplayCommandBase.take_action() returns two tuples
-        self.cmd.take_action(parsed_args)
-        mock_run_command.assert_called_with(
-            ['sudo', 'yum', 'upgrade', '-y',
              'python2-tripleoclient',
              'openstack-tripleo-common',
              'openstack-tripleo-heat-templates',
