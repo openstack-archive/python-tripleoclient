d77b63be8095bbefb5e5a9566821e95562b125c6
Juan Pablo Lopez <jlopezgu@redhat.com>
2021-01-20 10:27:13 +0000

    Modifying Openstack client for undercloud and overcloud backup
    - '--init' flag was marked with WARNING indicating it will be
      deprecated and replaced by '--setup-nfs' and '--setup-rear'.
      Now it receives arguments: 'rear' and 'nfs'. Defaults to: rear,
      so it doesn't break interface.
    - '--setup-nfs' flag added which installs and configure NFS
      on the 'BackupNode' host.
    - '--setup-rear' flag added which installs and configure ReaR
      on the 'Undercloud' or the 'Controller' nodes.
    - '--extra-vars' was enabled to pass variables to the playbooks
      by specifying JSON/YAML string or full path of the file.
    - Now 'tags' parameter is used internally to specify which
      tasks to run.
    - '--storage-ip' flag on overcloud backup command was marked with
      WARNING indicating it will be deprecated in favor of '--extra-vars'
      which allows to pass this and more properties.
      Thought, it has precedence over '--extra-vars'. If used it will replace
      the '--extra-vars' property 'tripleo_backup_and_restore_nfs_server'.
    
    Bugzilla: #1824806
    Change-Id: I31f2a554b03677fe29bd1186dee5d278b6d0554b
    (cherry picked from commit 945882fe726c66a9bf6b1f8f39d61141ebc38f78)

--- tripleoclient/v1/overcloud_backup.py
+++ tripleoclient/v1/overcloud_backup.py
@@ -15,6 +15,8 @@
 
 import argparse
 import logging
+import os
+import yaml
 
 from osc_lib.command import command
 from osc_lib.i18n import _
@@ -37,62 +39,167 @@ class BackupOvercloud(command.Command):
 
         parser.add_argument(
             '--init',
+            const='rear',
+            nargs='?',
+            action='store',
+            help=_("Initialize environment for backup,"
+                   "using 'rear' or 'nfs' as args "
+                   "which will check for package install "
+                   "and configured ReaR or NFS server. "
+                   "Defaults to: rear. "
+                   "i.e. --init rear. "
+                   "WARNING: This flag will be deprecated"
+                   "and replaced by '--setup-rear' and"
+                   "'--setup-nfs'.")
+        )
+
+        parser.add_argument(
+            '--setup-nfs',
             default=False,
             action='store_true',
-            help=_("Initialize enviornment for backup,"
-                   "which will check for package install"
-                   "status and configured ReaR.")
+            help=_("Setup the NFS server on the backup node "
+                   "which will install required packages "
+                   "and configuration on the host 'BackupNode' "
+                   "in the ansible inventory.")
+        )
+
+        parser.add_argument(
+            '--setup-rear',
+            default=False,
+            action='store_true',
+            help=_("Setup ReaR on the overcloud 'Controller' hosts which will "
+                   "install and configure ReaR.")
         )
 
         parser.add_argument(
             '--inventory',
             default='/home/stack/tripleo-inventory.yaml',
-            help=_("Tripleo inventory file generated with"
-                   "tripleo-ansible-inventory command.")
+            help=_("Tripleo inventory file generated with "
+                   "tripleo-ansible-inventory command. "
+                   "Defaults to: /home/stack/tripleo-inventory.yaml.")
         )
 
         parser.add_argument(
             '--storage-ip',
-            help=_("Storage IP is an optional parameter"
-                   "which allows for an ip of a storage"
-                   "server to be specified, overriding the"
-                   "default undercloud.")
+            help=_("Storage IP is an optional parameter "
+                   "which allows for an ip of a storage "
+                   "server to be specified, overriding the "
+                   "default undercloud. "
+                   "WARNING: This flag will be deprecated in "
+                   "favor of '--extra-vars' which will allow "
+                   "to pass this and other variables.")
+        )
+
+        parser.add_argument(
+            '--extra-vars',
+            default=None,
+            action='store',
+            help=_("Set additional variables as Dict or as "
+                   "an absolute path of a JSON or YAML file type. "
+                   "i.e. --extra-vars '{\"key\": \"val\", "
+                   " \"key2\": \"val2\"}' "
+                   "i.e. --extra-vars /path/to/my_vars.yaml "
+                   "i.e. --extra-vars /path/to/my_vars.json. "
+                   "For more information about the variables that "
+                   "can be passed, visit: https://opendev.org/openstack/"
+                   "tripleo-ansible/src/branch/master/tripleo_ansible/"
+                   "roles/backup_and_restore/defaults/main.yml.")
         )
 
         return parser
 
-    def _run_backup_Overcloud(self, parsed_args):
+    def _parse_extra_vars(self, raw_extra_vars):
+
+        if raw_extra_vars is None:
+            return {}
+        elif os.path.exists(raw_extra_vars):
+            with open(raw_extra_vars, 'r') as fp:
+                extra_vars = yaml.safe_load(fp.read())
+        else:
+            try:
+                extra_vars = yaml.safe_load(raw_extra_vars)
+            except yaml.YAMLError as exc:
+                raise RuntimeError(
+                    _('--extra-vars is not an existing file and cannot be '
+                      'parsed as YAML / JSON: %s') % exc)
+
+        return extra_vars
+
+    def _run_backup_overcloud(self, parsed_args):
         """Backup defined overcloud nodes."""
 
-        if parsed_args.init is False:
-            playbook = 'cli-overcloud-backup.yaml'
-            skip_tags = None
-        elif parsed_args.init is True:
-            playbook = 'prepare-overcloud-backup.yaml'
-            skip_tags = 'bar_create_recover_image, bar_setup_nfs_server'
+        extra_vars = self._parse_extra_vars(parsed_args.extra_vars)
 
         if parsed_args.storage_ip:
-            extra_vars = {
-                "tripleo_backup_and_restore_nfs_server": parsed_args.storage_ip
-                }
-        else:
-            extra_vars = None
+            storage_ip = parsed_args.storage_ip
 
-        LOG.debug(_('Starting Overcloud Backup'))
+            extra_vars[
+                'tripleo_backup_and_restore_nfs_server'
+            ] = storage_ip
+
+        if parsed_args.setup_nfs is True or parsed_args.init == 'nfs':
+
+            LOG.debug(_('Setting up NFS Backup node'))
+            self._run_ansible_playbook(
+                              playbook='prepare-nfs-backup.yaml',
+                              inventory=parsed_args.inventory,
+                              tags='bar_setup_nfs_server',
+                              skip_tags=None,
+                              extra_vars=extra_vars
+                              )
+
+        if parsed_args.setup_rear is True or parsed_args.init == 'rear':
+
+            LOG.debug(_('Installing ReaR on controller nodes'))
+            self._run_ansible_playbook(
+                              playbook='prepare-overcloud-backup.yaml',
+                              inventory=parsed_args.inventory,
+                              tags='bar_setup_rear',
+                              skip_tags=None,
+                              extra_vars=extra_vars
+                              )
+
+        if (parsed_args.setup_nfs is False and
+           parsed_args.setup_rear is False and
+           parsed_args.init is None):
+
+            LOG.debug(_('Starting Overcloud Backup'))
+            self._run_ansible_playbook(
+                              playbook='cli-overcloud-backup.yaml',
+                              inventory=parsed_args.inventory,
+                              tags='bar_create_recover_image',
+                              skip_tags=None,
+                              extra_vars=extra_vars
+                              )
+
+    def _run_ansible_playbook(self,
+                              playbook,
+                              inventory,
+                              tags,
+                              skip_tags,
+                              extra_vars):
+        """Run ansible playbook"""
+
         with utils.TempDirs() as tmp:
             utils.run_ansible_playbook(
                 playbook=playbook,
-                inventory=parsed_args.inventory,
+                inventory=inventory,
                 workdir=tmp,
                 playbook_dir=constants.ANSIBLE_TRIPLEO_PLAYBOOKS,
+                tags=tags,
                 skip_tags=skip_tags,
                 verbosity=utils.playbook_verbosity(self=self),
                 extra_vars=extra_vars
-                )
+            )
 
     def take_action(self, parsed_args):
 
-        self._run_backup_Overcloud(parsed_args)
+        if parsed_args.init:
+
+            LOG.warning("The following flags will be deprecated:"
+                        "[--init, --storage-ip]")
+
+        self._run_backup_overcloud(parsed_args)
         print(
             '\n'
             ' #############################################################\n'
