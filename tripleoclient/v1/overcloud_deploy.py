f44c7e25ed29d1217cf95944d29bee7978c7a8a2
Harald Jensås <hjensas@redhat.com>
2021-06-04 18:02:37 +0200

    Move copy_env_files to utils
    Use os.path.join purely to join the path, use of
    format is not required.
    
    Change-Id: I20db5526cb9c805cd65be154caff1eee0c66b3c1

--- tripleoclient/v1/overcloud_deploy.py
+++ tripleoclient/v1/overcloud_deploy.py
@@ -260,7 +260,7 @@ class DeployOvercloud(command.Command):
                 cleanup=(not parsed_args.no_cleanup))
 
         # Copy the env_files to tmp folder for archiving
-        self._copy_env_files(env_files, new_tht_root)
+        utils.copy_env_files(env_files, new_tht_root)
 
         if parsed_args.limit:
             # check if skip list is defined while using --limit and throw a
@@ -303,24 +303,6 @@ class DeployOvercloud(command.Command):
             deployment_options=deployment_options)
 
         self._unprovision_baremetal(parsed_args)
-
-    def _copy_env_files(self, files_dict, tht_root):
-        file_prefix = "file://"
-
-        for fullpath in files_dict.keys():
-
-            if not fullpath.startswith(file_prefix):
-                continue
-
-            path = fullpath[len(file_prefix):]
-
-            if path.startswith(tht_root):
-                continue
-
-            reloc_path = os.path.join(
-                tht_root,
-                "user-environments/{}".format(os.path.basename(path)))
-            utils.safe_write(reloc_path, files_dict[fullpath])
 
     def _try_overcloud_deploy_with_compat_yaml(self, tht_root, stack,
                                                stack_name,
