4203d1792e66ba5a8c04491154d585d3b496dbfa
Juan Larriba <jlarriba@redhat.com>
2021-07-14 13:01:13 +0200

    Allow for cron programming of backups
    Normally, backups are not done interactively, but during nights or
    weekends. This patch introduces the capacity of introducing in an easy
    way cron programming that will execute backups in predictable intervals.
    
    Closes-Bug: #1935675
    
    Depends-on: I86b3fb2c3d846bcd56859e0d572f1fe4d5e148fa
    Change-Id: I28219759922b7407121a3fa4d2f49ee1f4b8a3cd
    (cherry picked from commit c5347fbac354099852ba9d2f7291485704f7922d)

--- tripleoclient/v1/undercloud_backup.py
+++ tripleoclient/v1/undercloud_backup.py
@@ -75,6 +75,16 @@ class BackupUndercloud(command.Command):
         )
 
         parser.add_argument(
+            '--cron',
+            default=False,
+            action='store_true',
+            help=_("Sets up a new cron job that by default will "
+                   "execute a weekly backup at Sundays midnight, "
+                   "but that can be customized by using the "
+                   "tripleo_backup_and_restore_cron extra-var.")
+        )
+
+        parser.add_argument(
             '--db-only',
             default=False,
             action='store_true',
@@ -189,8 +199,19 @@ class BackupUndercloud(command.Command):
                                   extra_vars=extra_vars
                                   )
 
+            if parsed_args.cron is True:
+
+                self._run_ansible_playbook(
+                                playbook='cli-undercloud-backup-cron.yaml',
+                                inventory=parsed_args.inventory,
+                                tags=None,
+                                skip_tags=None,
+                                extra_vars=extra_vars
+                                )
+
             if (parsed_args.setup_nfs is False and
                parsed_args.setup_rear is False and
+               parsed_args.cron is False and
                parsed_args.init is None):
 
                 self._run_ansible_playbook(
