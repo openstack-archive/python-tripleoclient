bf7392db2ed2cc2906e1273b222ad4d1156c9cba
Cédric Jeanneret <cjeanner@redhat.com>
2021-08-02 13:46:39 +0200

    Make the "success" message more "success"-like
    With the current way, the ending message is a bit too similar when we
    get a success or a failure:
    "with error" compared to "without error" is too close, and it's easy to
    overlook an potential issue.
    
    This change changes "without error" to "successfully", making a clear
    difference.
    
    This issue was reported as-is on Red Hat Bugzilla:
    https://bugzilla.redhat.com/show_bug.cgi?id=1937226
    
    Change-Id: I059de98ef3fbb8f39a4119dbf334be4b14d989ad
    (cherry picked from commit 6780cd74a0a6c894215f68870a033fc7349167e6)

--- tripleoclient/v1/overcloud_deploy.py
+++ tripleoclient/v1/overcloud_deploy.py
@@ -1153,7 +1153,7 @@ class DeployOvercloud(command.Command):
                 'Only plan update is not supported.')
 
         deploy_status = 'DEPLOY_SUCCESS'
-        deploy_message = 'without error'
+        deploy_message = 'successfully'
 
         self._setup_clients(parsed_args)
 
