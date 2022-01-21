e3c66d6a2065a0be973dd014973717524bb1e456
Harald Jensås <hjensas@redhat.com>
2022-01-21 22:41:45 +0100

    node export provisioned - get role_data from stack
    Since change: I1efecdcd7afa6af3e6b4b26f4435198836db535f
    we store the THT Jinja2 input files in the stack output.
    
    Make the --roles-file option optional for overcloud node
    export provisioned commend. Use the value from the stack
    if --roles-file not provided by the user.
    
    Change-Id: I9e2a6d6d45b86da333658a73179607b2623b5cc7

--- tripleoclient/tests/v1/overcloud_node/test_overcloud_node.py
+++ tripleoclient/tests/v1/overcloud_node/test_overcloud_node.py
@@ -900,6 +900,13 @@ class TestExtractProvisionedNode(test_utils.TestCommand):
 
         self.cmd = overcloud_node.ExtractProvisionedNode(self.app, None)
 
+        roles_data = [
+            {'name': 'Controller',
+             'default_route_networks': ['External'],
+             'networks_skip_config': ['Tenant']},
+            {'name': 'Compute'}
+        ]
+
         self.stack_dict = {
             'parameters': {
                 'ComputeHostnameFormat': '%stackname%-novacompute-%index%',
@@ -908,6 +915,12 @@ class TestExtractProvisionedNode(test_utils.TestCommand):
                 'ControllerNetworkConfigTemplate': 'templates/controller.j2'
             },
             'outputs': [{
+                'output_key': 'TripleoHeatTemplatesJinja2RenderingDataSources',
+                'output_value': {
+                    'roles_data': roles_data,
+                    'networks_data': {}
+                }
+            }, {
                 'output_key': 'AnsibleHostVarsMap',
                 'output_value': {
                     'Compute': [
@@ -1038,12 +1051,6 @@ class TestExtractProvisionedNode(test_utils.TestCommand):
             mode='w', delete=False, suffix='.yaml')
         self.extract_file.close()
 
-        roles_data = [
-            {'name': 'Controller',
-             'default_route_networks': ['External'],
-             'networks_skip_config': ['Tenant']},
-            {'name': 'Compute'}
-        ]
         self.roles_file = tempfile.NamedTemporaryFile(
             mode='w', delete=False, suffix='.yaml')
         self.roles_file.write(yaml.safe_dump(roles_data))
@@ -1059,12 +1066,10 @@ class TestExtractProvisionedNode(test_utils.TestCommand):
 
         self.baremetal.node.list.return_value = self.nodes
 
-        argslist = ['--roles-file', self.roles_file.name,
-                    '--output', self.extract_file.name,
+        argslist = ['--output', self.extract_file.name,
                     '--yes']
         self.app.command_options = argslist
-        verifylist = [('roles_file', self.roles_file.name),
-                      ('output', self.extract_file.name),
+        verifylist = [('output', self.extract_file.name),
                       ('yes', True)]
 
         parsed_args = self.check_parser(self.cmd,
